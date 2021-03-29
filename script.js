c257d6718851bfef79a528657d59166563153f5e
Lilala-Lee <74852555+Lilala-Lee@users.noreply.github.com>
2021-03-29 16:34:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principle = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
 }
-        
+
+var slide = document.getElementById('slide'),
+    sliderDiv = document.getElementById("sliderAmount");
+
+slide.onchange = function() {
+    sliderDiv.innerHTML = this.value;
+}
